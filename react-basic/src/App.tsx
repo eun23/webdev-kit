57186aae1332f03f33e3a2ff0108388620d7aed5
eun23 <127043779+eun23@users.noreply.github.com>
2025-02-25 17:41:16 +0900

    2025.02.25

--- react-basic/src/App.tsx
+++ react-basic/src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import Component, { ClassComponent, FunctionalComponent } from 
+'./component/Component'
 import './App.css';
+import CurlyBraces from './component/CurlyBraces';
+import Properties from './component-manage/Properties';
+import ConditionRender from './component-manage/ConditionRender';
 
 function App() {
   return (
     <div>
-      
+      {/* <ClassComponent />
+      <FunctionalComponent />
+      <Component /> */}
+      {/* <CurlyBraces /> */}
+      {/* <Properties /> */}
+      <ConditionRender />
     </div>
   );
 }
